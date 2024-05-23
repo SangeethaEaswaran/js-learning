2f8dd2a36526e331329c7046a5236ff594dedf7d
Sangeetha Easwaran <sangeethaeaswaran01@gmail.com>
2024-05-23 18:48:07 +0100

    add js learning

---
+++ lessons/lesson9.js
@@ -0,0 +1,7 @@
+// import { CustomerDetails } from '../helpers/printHelper.js'
+import { custDetails } from '../helpers/printHelper.js'
+
+// var custDetails = new CustomerDetails();
+custDetails.printFirstName('John')
+custDetails.printLastName('smith')
+
